b5ced229f4a31d2b724fd4b46a3e335c0bfd0779
Christian Werner <cw@rizzistrasse.com>
2019-06-26 16:48:32 +0200

    loading of Entrys

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -26,26 +26,31 @@ export class ListPage implements OnInit {
     public items: Array<{ title: string; note: string; icon: string }> = [];
 
     constructor(public http: HttpClient) {
-        console.log('aaa', this.load());
-        for (let i = 1; i < 11; i++) {
-            this.items.push({
-                title: 'Item ' + i,
-                note: 'This is item #' + i,
-                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-            });
-        }
+        this.load();
+
     }
 
     load() {
         const headers = new Headers();
 
-        const apiendpoint = 'http://127.0.0.1:8080/wohnung/get/1';
-        console.log(apiendpoint);
+        const apiendpoint = 'http://127.0.0.1:8080/wohnung/all';
+
         return this.http.get(apiendpoint).subscribe(data => {
-            console.log(data);
+            this.buildList(data);
         });
 
 
+    }
+
+    buildList(data) {
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            this.items.push({
+                title: 'Item ' + data[i].id + ' ' + data[i].country,
+                note: 'This is item #' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
     }
 
     // add back when alpha.4 is out
