604310fe21e3e3ddb90a8ecc3baffa3824b4b168
ToResel <33777851+ToResel@users.noreply.github.com>
2019-06-26 18:40:02 +0200

    more more work

---
+++ src/app/wohnung/wohnung.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wohnung',
+  templateUrl: './wohnung.page.html',
+  styleUrls: ['./wohnung.page.scss'],
+})
+export class WohnungPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
