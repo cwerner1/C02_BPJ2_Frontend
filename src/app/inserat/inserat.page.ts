edc4b5d690ad4ad3e77692ccb5d42ede5a060f84
Christian Werner <cw@rizzistrasse.com>
2019-06-26 17:37:05 +0200

    adding Wohnung

--- src/app/inserat/inserat.page.ts
+++ src/app/inserat/inserat.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-inserat',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InseratPage implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+  }
 
   ngOnInit() {
   }
 
+  sendPostRequest() {
+    const headers = new HttpHeaders();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const postData = {
+      'user_id': 1,
+      'country': 'Austriaa',
+      'city': 'city',
+      'description': 'customer004@email.com',
+      'roomCount': '0000252525'
+    };
+
+    this.http.post('http://127.0.0.1:8080/wohnung/add', postData, {headers: headers})
+        .subscribe(data => {
+          console.log(data);
+        }, error => {
+          console.error(error);
+        });
+  }
 }
